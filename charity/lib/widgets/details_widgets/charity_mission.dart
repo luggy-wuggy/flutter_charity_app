44f62cd2960ff20ca14fa956e28b2bda7b2ddc69
Luqman Abdurrohman <luqmanabdurrohman@Jyst-Macbook-Air.local>
2022-01-10 18:47:15 -0800

    charity mission ui

--- charity/lib/widgets/details_widgets/charity_mission.dart
+++ charity/lib/widgets/details_widgets/charity_mission.dart
@@ -14,47 +14,11 @@ class CharityMission extends StatefulWidget {
   State<CharityMission> createState() => _CharityMissionState();
 }
 
-class _CharityMissionState extends State<CharityMission>
-    with TickerProviderStateMixin<CharityMission> {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-  late String firstHalf;
-  late String secondHalf;
-
-  @override
-  void initState() {
-    super.initState();
-    _seperateText();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOutBack,
-    );
-  }
-
-  _seperateText() {
-    if (widget.mission.length > 200) {
-      firstHalf = widget.mission.substring(0, 200) + "...";
-      secondHalf = widget.mission.substring(200, widget.mission.length);
-    } else {
-      firstHalf = widget.mission;
-      secondHalf = "";
-    }
-  }
-
+class _CharityMissionState extends State<CharityMission> {
   _toggleContainer() {
-    print(_animation.status);
     setState(() {
       widget.isExpanded = !widget.isExpanded;
     });
-    if (_animation.status != AnimationStatus.completed) {
-      _controller.forward();
-    } else {
-      _controller.animateBack(0, duration: const Duration(seconds: 1));
-    }
   }
 
   @override
@@ -74,120 +38,100 @@ class _CharityMissionState extends State<CharityMission>
               ),
             ),
           ),
-          const SizedBox(height: 5),
-          // Text(
-          //   firstHalf,
-          //   style: GoogleFonts.lato(
-          //     textStyle: const TextStyle(
-          //       color: Colors.black,
-          //       fontSize: 14,
-          //     ),
-          //   ),
-          // ),
-          SizeTransition(
-            sizeFactor: _animation,
-            axis: Axis.vertical,
-            axisAlignment: 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.mission,
+          AnimatedSize(
+            duration: Duration(milliseconds: 500),
+            alignment: Alignment.topCenter,
+            curve: Curves.easeInOutBack,
+            child: Container(
+              constraints: widget.isExpanded
+                  ? const BoxConstraints(maxHeight: double.infinity)
+                  : const BoxConstraints(maxHeight: 50),
+              child: Text(widget.mission, overflow: TextOverflow.clip),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 400),
+                curve: Curves.easeInOutSine,
+                opacity: widget.isExpanded ? 1 : 0,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 400),
+                  curve: Curves.easeInOutSine,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: widget.isExpanded ? 8 : 0,
+                    vertical: widget.isExpanded ? 4 : 0,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF57b894),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: AnimatedDefaultTextStyle(
+                    duration: Duration(milliseconds: 400),
+                    curve: Curves.easeInOutSine,
+                    style: GoogleFonts.lato(
+                      color: Colors.white,
+                      fontSize: widget.isExpanded ? 12 : 0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    child: const Text("Website"),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 8),
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 400),
+                opacity: widget.isExpanded ? 1 : 0,
+                curve: Curves.easeInOutSine,
+                child: AnimatedDefaultTextStyle(
+                  duration: Duration(milliseconds: 400),
+                  curve: Curves.easeInOutSine,
+                  style: GoogleFonts.lato(
+                    color: Colors.black,
+                    fontSize: widget.isExpanded ? 12 : 0,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  child: Text(
+                    "Is this your organization? ",
+                  ),
+                ),
+              ),
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 400),
+                opacity: widget.isExpanded ? 1 : 0,
+                curve: Curves.easeInOutSine,
+                child: AnimatedDefaultTextStyle(
+                  duration: Duration(milliseconds: 400),
+                  curve: Curves.easeInOutSine,
+                  style: GoogleFonts.lato(
+                    color: Colors.blue,
+                    fontSize: widget.isExpanded ? 12 : 0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Text("Contact us!"),
+                ),
+              ),
+              const Spacer(),
+              GestureDetector(
+                onTap: () {
+                  _toggleContainer();
+                },
+                child: Text(
+                  widget.isExpanded ? 'less' : 'more',
                   style: GoogleFonts.lato(
                     textStyle: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
+                      decoration: TextDecoration.underline,
+                      color: Colors.blue,
+                      fontSize: 13,
                     ),
                   ),
                 ),
-                const SizedBox(height: 10),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 4),
-                      decoration: const BoxDecoration(
-                        color: Color(0xFF57b894),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        // boxShadow: [
-                        //   BoxShadow(
-                        //     blurRadius: 3,
-                        //     spreadRadius: 1,
-                        //     offset: Offset(-2, 2),
-                        //     color: Color.fromARGB(255, 214, 211, 211),
-                        //   )
-                        // ],
-                      ),
-                      child: Text(
-                        "Website",
-                        style: GoogleFonts.lato(
-                          color: Colors.white,
-                          fontSize: 10,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      "Is this your organization? ",
-                      style: GoogleFonts.lato(
-                        color: Colors.black,
-                        fontSize: 10,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    Text(
-                      "Contact us!",
-                      style: GoogleFonts.lato(
-                        color: Colors.blue,
-                        fontSize: 10,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          // AnimatedSize(
-          //   duration: const Duration(milliseconds: 500),
-          //   child: ConstrainedBox(
-          //     constraints: widget.isExpanded
-          //         ? new BoxConstraints()
-          //         : new BoxConstraints(maxHeight: 50.0),
-          //     child: Text(
-          //       widget.mission,
-          //       style: GoogleFonts.lato(
-          //         textStyle: const TextStyle(
-          //           color: Colors.black,
-          //           fontSize: 14,
-          //         ),
-          //       ),
-          //       softWrap: true,
-          //       overflow: TextOverflow.ellipsis,
-          //     ),
-          //   ),
-          // ),
-          GestureDetector(
-            onTap: () {
-              _toggleContainer();
-            },
-            child: Text(
-              widget.isExpanded ? 'less' : 'more',
-              style: GoogleFonts.lato(
-                textStyle: const TextStyle(
-                  decoration: TextDecoration.underline,
-                  color: Colors.blue,
-                  fontSize: 13,
-                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
